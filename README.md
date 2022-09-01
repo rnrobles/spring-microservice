baecb666e3b4d8d8fff7847cee7641cd77ca935b
Ramón Natanael Robles Hermosillo <rnrobles.dev@gmail.com>
2022-09-01 16:50:27 -0500

    eureka server

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Practicas de microservicios
 
 # Config server
 port 1234
+
+
+# Eureka server
+port 8761
---
+++ ServerRegistryDiscovery/src/test/java/online/rnrobles/serverdiscovery/ServerRegistryDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package online.rnrobles.serverdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
